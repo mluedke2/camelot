a60036419472437dacbd160537bc4db64ce5c529
mluedke2 <mluedke2@gmail.com>
2015-09-02 23:48:43 -0700

    save sound and play back with button

--- Camelot/ViewController.swift
+++ Camelot/ViewController.swift
@@ -6,9 +6,13 @@
 //  Copyright (c) 2015 Razeware. All rights reserved.
 //
 
+import AVFoundation
 import ResearchKit
 
 class ViewController: UIViewController, ORKTaskViewControllerDelegate {
+  
+  var audioPlayer: AVAudioPlayer?
+  var soundFileURL: NSURL?
   
   @IBAction func consentTapped(sender : AnyObject) {
     let taskViewController = ORKTaskViewController(task: ConsentTask, taskRunUUID: nil)
@@ -29,9 +33,28 @@ class ViewController: UIViewController, ORKTaskViewControllerDelegate {
     presentViewController(taskViewController, animated: true, completion: nil)
   }
   
+  @IBAction func playMostRecentSound(sender: AnyObject) {
+    if let soundFileURL = soundFileURL {
+      audioPlayer = AVAudioPlayer(contentsOfURL: soundFileURL, fileTypeHint: AVFileTypeAppleM4A, error: nil)
+      audioPlayer?.play()
+    }
+  }
+  
   func taskViewController(taskViewController: ORKTaskViewController, didFinishWithReason reason: ORKTaskViewControllerFinishReason, error: NSError?) {
-    //TODO: handle results with taskViewController.result
+    
+    // check for a sound file
+    soundFileURL = findSoundFile(taskViewController.result)
+    
     taskViewController.dismissViewControllerAnimated(true, completion: nil)
   }
+  
+  func findSoundFile(result: ORKTaskResult) -> NSURL? {
+    
+    if let results = result.results, let stepResult = results[3] as? ORKStepResult, let subResults = stepResult.results, let fileResult = subResults[0] as? ORKFileResult {
+      
+      return fileResult.fileURL
+    }
+    
+    return nil
+  }
 }
-
